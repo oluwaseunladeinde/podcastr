7a9e5c40a8457e3a6b936827da22b3e63ef2ef82
oluwaseunladeinde <oluwaseun.ladeinde@yahoo.com>
2024-10-03 16:34:45 +0100

    first commit

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,12 @@
+import { PodcastCard } from "@/components/podcast-card"
+import { PODCAST_DATA } from "@/constants"
 
 const Home = () => {
   return (
     <div className='mt-9 flex flex-col gap-9'>
       <section className='flex flex-col gap-5'>
         <h1 className='text-20 font-bold text-white-1'>Trending Podcast</h1>
-
+        {PODCAST_DATA.map((podcast) => (<PodcastCard key={podcast.id} />))}
       </section>
     </div>
   )
